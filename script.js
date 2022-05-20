e2f5c9668b10979ca1d505b0a714bf74cbce56cd
Alexandre <xandre2setembro2002@hotmail.com>
2022-05-19 21:29:44 -0300

    added calculate functions

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ btn3.addEventListener("click", function check3(){
 
 //input only numbers and "."
 
+
 function isNumberKey(evt)
 {
 	var charCode = (evt.which) ? evt.which : evt.keyCode
@@ -72,24 +73,71 @@ clear.addEventListener("click", function(){
     for(i = 0; i < mtrxnums3.length; i++){
         mtrxnums3[i].value = ""
     };
+    result.style.display = "none"
 });
 
-//calculate
+//calculate vars
+
+var oneone2x2 = document.getElementById("211").value;
+var onetwo2x2 = document.getElementById("212").value;
+
+var twoone2x2 = document.getElementById("221").value;
+var twotwo2x2 = document.getElementById("222").value;
+
+
+var oneone3x3 = document.getElementById("311").value;
+var onetwo3x3 = document.getElementById("312").value;
+var onethree3x3 = document.getElementById("313").value;
+
+var twoone3x3 = document.getElementById("321").value;
+var twotwo3x3 = document.getElementById("322").value;
+var twothree3x3 = document.getElementById("323").value;
+
+var threeone3x3 = document.getElementById("331").value;
+var threetwo3x3 = document.getElementById("332").value;
+var threethree3x3 = document.getElementById("333").value;
+
+var diagonals = {
+    "dia1": (oneone3x3 *  twotwo3x3 * threethree3x3),
+    "dia2": (onetwo3x3 * twothree3x3 * threeone3x3),
+    "dia3": (onethree3x3 * twoone3x3 * threetwo3x3),
+    "diaMinus1": (onethree3x3 * twotwo3x3 * threeone3x3),
+    "diaMinus2": (oneone3x3 * twothree3x3 * threetwo3x3),
+    "diaMinus3": (onetwo3x3 * twoone3x3 * threethree3x3)
+}
+
+
+//calculate functions
+
 
 function cal1(){
     if(mtrxnums1.value == ""){alert("Please fill the input.")}
     else{
         result.style.display = "block"
-        result.innerText = "Det = " + mtrxnums1.value;
+        result.innerText = "Det = " + mtrxnums1[0].value;
     };
 };
 
 function cal2(){
-
-};
+    for(var j = 0; j < mtrxnums2.length; j++){
+        if(mtrxnums2[j].value == ""){
+            alert("Please fill the blank inputs.");
+            break;
+        }};
+    var det = (oneone2x2 * twotwo2x2) - (onetwo2x2 * twoone2x2);
+    result.style.display = "block";
+    result.innerText = "Det = " + det;
+        };
 
 function cal3(){
-
+    for(var j = 0; j < mtrxnums3.length; j++){
+        if(mtrxnums3[j].value == ""){
+            alert("Please fill the blank inputs.");
+            break;
+        }};
+        var det = (diagonals.dia1 + diagonals.dia2 + diagonals.dia3) - (diagonals.diaMinus1 + diagonals.diaMinus2 + diagonals.diaMinus3)
+        result.style.display = "block";
+        result.innerText = "Det = " + det;
 };
 
 cal.addEventListener("click", function(){ 
